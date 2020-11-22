6d52094b1841f6f48fff179bac7d39349db85a2a
sergio sacristan santero <srg_ek@hotmail.com>
2020-11-22 20:54:58 +0100

    [angular] - Route

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,18 +2,34 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from '@angular/core';
 import { AppComponent } from './app.component';
-import { Componente1 } from './components/componente1/componente1.component';
-import { FilmsComponent } from './components/films/films.component';
-import { MenuComponent } from './components/menu/menu.component';
-import { FooterComponent } from './components/footer/footer.component';
-import { MailComponent } from './components/mail/mail.component';
+import { Componente1 } from '@components//componente1/componente1.component';
+import { FilmsComponent } from '@components//films/films.component';
+import { MenuComponent } from '@components//menu/menu.component';
+import { FooterComponent } from '@components//footer/footer.component';
 import { HttpClientModule } from '@angular/common/http';
-import { EntradasSalidasComponent } from './components/entradas-salidas/entradas-salidas.component';
-import { UserComponent } from './components/user/user.component';
-import { FormulariosComponent } from './components/formularios/formularios.component';
+import { EntradasSalidasComponent } from '@components//entradas-salidas/entradas-salidas.component';
+import { UserComponent } from '@components//user/user.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { Directiva1Directive } from './directives/directiva1.directive';
+import { RouterModule, Routes } from '@angular/router';
+import { FormsComponent } from '@components/forms/forms.component';
+import { DirectivesComponent } from './components/directives/directives.component';
 
+const rutas: Routes = [
+
+  {
+    path: 'film',
+    component: FilmsComponent
+  },
+  {
+    path: 'forms',
+    component: FormsComponent
+  },
+  {
+    path: 'directives',
+    component: Directiva1Directive
+  }
+]
 
 @NgModule({
   declarations: [
@@ -22,16 +38,17 @@ import { Directiva1Directive } from './directives/directiva1.directive';
     FilmsComponent,
     MenuComponent,
     FooterComponent,
-    MailComponent,
     EntradasSalidasComponent,
     UserComponent,
-    FormulariosComponent,
-    Directiva1Directive
+    FormsComponent,
+    Directiva1Directive,
+    DirectivesComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    RouterModule.forRoot(rutas)
   ],
   providers: [],
   bootstrap: [AppComponent],
