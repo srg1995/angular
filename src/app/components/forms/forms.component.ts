d097f2a9907c8a88b48005c93417ebbd12542965
sergio sacristan santero <srg_ek@hotmail.com>
2020-12-10 18:33:42 +0100

    [angular] - decorador @ViewChild

--- src/app/components/forms/forms.component.ts
+++ src/app/components/forms/forms.component.ts
@@ -1,33 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MailService } from '@services/mail/mail.service';
 
 @Component({
-  selector: 'app-forms',
-  templateUrl: './forms.component.html',
-  styleUrls: ['./forms.component.css']
+	selector: 'app-forms',
+	templateUrl: './forms.component.html',
+	styleUrls: ['./forms.component.css']
 })
 export class FormsComponent implements OnInit {
 
-  signupForm: FormGroup
-  constructor(
-    private _builder: FormBuilder,
-    private _smail: MailService
-  ) {
-    this.signupForm = this._builder.group({
-      emisor: ['', Validators.compose([Validators.email, Validators.required])],
-      asunto: ['', Validators.required],
-      mensaje: ['', Validators.required],
-    })
-  }
+	mailForm: FormGroup
+	constructor(
+		private _builder: FormBuilder,
+		private _smail: MailService
+	) {
+		this.mailForm = this._builder.group({
+			emisor: ['', Validators.compose(
+				[
+					Validators.email,
+					Validators.required
+				]
+			)],
+			asunto: ['',
+				Validators.required
+			],
+			mensaje: ['',
+				Validators.required
+			],
+		})
+	}
 
-  ngOnInit(): void {
-  }
+	@ViewChild('btn_envio', {static:true}) button:ElementRef;
+
+	ngOnInit(): void {
+		this.button.nativeElement.innerHTML = "Enviar"
+	}
 
 
-  send(values){
-    this._smail.envio(values.emisor, values.asunto, values.mensaje).subscribe(response =>{
-      console.log(response);
-    })
-  }
+	send(values) {
+		this._smail.envio(values.emisor, values.asunto, values.mensaje).subscribe(response => {
+			console.log(response);
+		})
+	}
 }
